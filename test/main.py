a5959f3e46709a96cb2daeedf6be3388d2daa02a
hathix <neelmehta@college.harvard.edu>
2015-04-22 00:09:25 -0400

    started bayesian probability tester

--- test/main.py
+++ test/main.py
@@ -32,3 +32,15 @@ print(words)
 [t.load_freq_list(words) for t in tweets]
 for t in tweets:
     print(t.freq_list)
+
+parties = [Party.Republican, Party.Democrat]
+tweets_by_party = [[tweet for tweet in tweets if tweet.party == party] for party in parties]
+# P(y = R) = len(tweets_by_party[Party.Republican]) / len(tweets)
+
+def prob(party, tweet):
+    prob_y = len(tweets_by_party[party]) / len(tweets)
+    numerator_probs = []
+    for i in xrange(0, len(tweet.freq_list)):
+        to_search = tweets_by_party[party]
+        numerator_probs[i] = len([comp for comp in to_search if comp.freq_list[i] == tweet.freq_list[i]]) / len(to_search)
+    # todo finish
