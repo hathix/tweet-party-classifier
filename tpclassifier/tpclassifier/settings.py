ef2cb3857f25af5595f9fa7ee3f923907cc45d4c
Ajay <ajaynathan@college.harvard.edu>
2015-04-23 03:21:00 -0400

    Added 1000 tweets to db and added simple frontend

--- tpclassifier/tpclassifier/settings.py
+++ tpclassifier/tpclassifier/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tweets'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'tpclassifier.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
     }
 }
 
--- tpclassifier/tpclassifier/urls.py
+++ tpclassifier/tpclassifier/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'tpclassifier.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^tweets/', include('tweets.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
