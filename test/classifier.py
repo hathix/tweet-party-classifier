8c848822c2c6dc3ce39375dd0cc4048ee642f4e6
hathix <neelmehta@college.harvard.edu>
2015-04-22 23:44:30 -0400

    working on writeup

--- test/classifier.py
+++ test/classifier.py
@@ -10,9 +10,23 @@ def prob(party, tweet):
     # numerator
     party_tweets = tweets_by_party[party]
     prob_of_party = len(party_tweets) / len(tweets)
-    feature_probs =
+    feature_probs = 
     numerator_probs = []
     for i in xrange(0, len(tweet.freq_list)):
         to_search = tweets_by_party[party]
         numerator_probs[i] = len([comp for comp in to_search if comp.freq_list[i] == tweet.freq_list[i]]) / len(to_search)
     # todo finish
+
+def bayes(party, party_tweets, all_tweets, sample_tweet):
+    prob_of_party = len(party_tweets) / len(all_tweets)
+    feature_probs = []
+    # iterate over every word (boolean flag)
+    for i in xrange(0, len(sample_tweet.freq_list)):
+        # P(A|B) = P(B|A) * P(A) / P(B)
+        # P(match|party) = P(party|match) * P(match) / P(party)
+        #                = P(match)
+        # because we're only looking at the party's tweets
+        matching_fn = lambda test_tweet: test_tweet.freq_list[i] == sample_tweet.freq_list[i]
+        matching_tweets = filter(matchin_fn, party_tweets)
+        probability = len(matching_tweets) / len(party_tweets)
+        feature_probs.append(probability)
