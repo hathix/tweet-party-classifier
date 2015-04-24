fccd4ec12285c5b3c5fef2b2527790470bace2d2
Samuel K. Lam <contact@samuelklam.com>
2015-04-23 22:01:29 -0400

    setting up scikit-learn, added in readme

---
+++ scikit-learn/README_sk_learn.txt
@@ -0,0 +1,28 @@
+# README #
+
+Implementation of a Naive Bayes classifier for political tweets using scikit-learn.
+
+
+### Why scikit-learn?
+
+Scikit-learn provides efficient and optimized out-of-the-box classification algorithms.
+
+It also provides lots of useful functionalities for evaluating and improving the quality of classification.
+
+Using scikit-learn helps to cut down on development time by sparing us the troule of having to implement the internal details of every algorithm, it also provides us with a simple and flexible framework to test future approaches.
+
+We hope to assess differences in accuracy between the scikit-learn implementation and our custom implementation.
+
+
+### How to setup
+
+The classification and feature extraction pipeline has been tested with the
+following library versions:
+
+- Python 2.7.6
+
+- nltk==3.0.2
+
+- scikit-learn==0.15.1
+
+- numpy==1.9.1
---
+++ scikit-learn/sk_learn_test/main.py
@@ -0,0 +1,47 @@
+import utils
+from tweet import Tweet
+from party import Party
+from classifier import Classifier
+
+# Here be testing
+
+'''
+corpus = [
+    "This could be paradise",
+    "Paradise? What's that?",
+    "Dreams of paradise",
+    "Welcome to Paradise",
+    "I'm just a believer",
+    "Believe in yourself"
+]
+sample = "Paradise but I give up"
+sample2 = "I'm just a believer"
+
+lis = most_common_words(corpus, 2)
+print(freq_list(sample, lis))
+print(freq_list(sample2, lis))
+'''
+
+# todo keep the @'s around
+tweets = [
+    Tweet(Party.Democrat, "B. Obama", "Economy economy economy jobs jobs jobs healthcare"),
+    Tweet(Party.Democrat, "H. Clinton", "..."),
+    Tweet(Party.Republican, "M. Romney", "Jobs jobs jobs guns"),
+    Tweet(Party.Republican, "J. McCain", "...")
+]
+cat = "Economy economy jobs healthcare guns"
+
+words = utils.most_common_words([t.text for t in tweets], 2)
+[t.load_freq_list(words) for t in tweets]
+
+classifier = Classifier(tweets, words)
+print(classifier.test(cat, Party.Republican))
+print(classifier.test(cat, Party.Democrat))
+
+if classifier.predict(cat) == Party.Republican:
+    print "Republican"
+else:
+    print "Democrat"
+
+testr = Tweet(Party.Democrat, "A. Traitor", "Jobs guns guns guns guns money")
+print(classifier.accuracy([testr]))
