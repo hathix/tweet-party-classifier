774957864188bc0256501f2244a94fa692eeffe9
Samuel K. Lam <contact@samuelklam.com>
2015-04-28 23:21:45 -0400

    added in test_classiifer

--- scikit-learn/sk_learn_test/party.py
+++
@@ -1,8 +0,0 @@
-class Party():
-    Democrat = 1
-    Republican = 0
-    
-def ind_to_party(i):
-    return {Party.Democrat: "Democrat",
-            Party.Republican: "Republican"
-            }[i]
